0efc1c38271fc5d0eb02ea82f00490c016ca0c85
Rayce Coyne <rayce.coyne@gmail.com>
2022-12-28 09:05:47 -0800

    add functionality to searchbar and videolist. embed video player

--- src/index.js
+++ src/index.js
@@ -1,30 +1,46 @@
+import _ from 'lodash';
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import SearchBar from './components/searchbar';
+import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 import YTSearch from 'youtube-api-search';
-import {KEY_YOUTUBE} from "./components/keys.js"
+import {KEY_YOUTUBE} from "./components/keys"
 
 
 //create new component. Should produce HTML
 class App extends Component {
     constructor(props){
         super(props);
-        this.state = {videos: []};
+        this.state = {
+            videos: [], 
+            selectedVideo: null
+        };
+        this.videoSearch('Phantom of the Opera');
+    }
+
+    videoSearch(term){
         YTSearch(
-            {key: KEY_YOUTUBE, term: 'surfboards'}, 
+            {key: KEY_YOUTUBE, term: term}, 
             //(videos)=>{this.setState({videos:videos});}
-            (videos)=>{this.setState({videos});}
+            (videos)=>{this.setState({
+                videos: videos,
+                selectedVideo: videos[0]}
+            );}
         );
     }
 
     render() {
+        const videoSearch = _.debounce((term)=> {this.videoSearch(term)},300);
         
         //Passing props from parent to child via jsx tag
         return (
             <div>
-                <SearchBar />
-                <VideoList videos={this.state.videos} />
+                <SearchBar onSearchTermChange={videoSearch}/>
+                <VideoDetail video={this.state.selectedVideo} />
+                <VideoList 
+                    onVideoSelect={selectedVideo=> this.setState({selectedVideo})}
+                    videos={this.state.videos} />
             </div>
         );
     }
